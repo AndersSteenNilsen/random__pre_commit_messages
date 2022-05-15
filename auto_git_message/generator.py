6a07484730825b0d86bcc4b7ec51429abc117d2a
Anders Steen <anders.steen@gsuite.knowit.no>
2022-05-15 19:25:52 +0200

    Adding diff

--- auto_git_message/generator.py
+++ auto_git_message/generator.py
@@ -1,13 +1,17 @@
-
 from ast import arg
 from typing import Sequence
+import sys
+import subprocess
 
 
-def generate_message(argv: Sequence[str] | None = None):
-    print(argv)
-    print('asdfasdfas')
-    return 1
+def generate_message(commit_message_filename=sys.argv[1]):
+    print(sys.argv[1:])
+    diff = subprocess.run(['git', 'diff'], capture_output=True).stdout
+    print(diff)
+    with open(commit_message_filename, 'w+') as f:
+        f.write('!')
+    return 0
 
 
-if __name__ ==  '__main__':
-    print(generate_message())
+if __name__ == '__main__':
+    exit()
